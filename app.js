361abfa172e6e894015d7b28d428b1b0f907bb71
Daedra <foglzerika@gmail.com>
2020-05-04 18:41:42 -0500

    feat | configure mysql server

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
-
+const mysql = require("mysql2");
 const app = express();
 
 const PORT = process.env.PORT;
 
-const db = require("./models");
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
+const mysqlConnection = mysql.createConnection({
+    host : process.env.HOST,
+    user : process.env.USERDB,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE,
+    multipleStatements : true
 });
+
+mysqlConnection.connect((err) =>{
+    if(!err){
+        console.log("connected");
+    }else{
+        console.log("Connection failed");
+    }
+});
+
 
 app.use(cors({
     credentials : true,
