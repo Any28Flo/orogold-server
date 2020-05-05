d85b7a1b26dcd69ad9bcb2cae42661ed4d370cb2
Daedra <foglzerika@gmail.com>
2020-05-05 02:54:00 -0500

    feat | Adding validations to appointment-routes /create

--- routes/appointment-routes.js
+++ routes/appointment-routes.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const appointmentRoutes = express.Router();
+const { check, validationResult } = require('express-validator');
 
 const mysqlConnection = require("./../config/db.config");
 
-appointmentRoutes.get("/list-appointment", (req, res) =>{
-    mysqlConnection.query("SELECT * from appointment", (err, rows, fields) =>{
-        if(!err){
-            res.send(rows);
-        }else{
-            console.log(err);
+appointmentRoutes.post("/create",[
+    check('email').isEmail(),
+    check('phone').isLength({ min: 7})
+
+
+],  (req, res) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    const{username,email,phone} = req.body;
+    mysqlConnection.query("INSERT INTO appointments(name,email,phone) VALUES (?,?,?)",[username,email,phone], function(error,results,fields){
+        if(error){
+            res.status(400).json({msg : `${JSON.stringify(errors)}`});
+            return;
+
         }
+        res.status(200).json({msg: "Sucess"});
+
     })
 });
 module.exports = appointmentRoutes;
